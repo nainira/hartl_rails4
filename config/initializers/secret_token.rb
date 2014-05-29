94f274849960828faf86fa3446d3048003879300
nainira <nainirajk@gmail.com>
2014-05-29 20:55:39 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HartlRails4::Application.config.secret_key_base = '95141d09acaefee05a5386a0a737cc71ad8f3b001d9efac92a130d3c4d8b14df9649a7c27ef4b8b632b40073c3ae4da7a71bf05ba66bc30cea0eca3de867dbf0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HartlRails4::Application.config.session_store :cookie_store, key: '_hartl_rails4_session'
